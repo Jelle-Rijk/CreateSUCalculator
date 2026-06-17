c561c7be66115098d35c7d294427ef13e4bee806
Jelle-Rijk <jellerijk@gmail.com>
2026-06-17 23:37:20 +0200

    Initial work on viewing a selected network.

--- src/main/java/org/jellerijk/mccreatecalc/main/CalculatorTestApplication.java
+++ src/main/java/org/jellerijk/mccreatecalc/main/CalculatorTestApplication.java
@@ -1,13 +1,17 @@
 package org.jellerijk.mccreatecalc.main;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import org.jellerijk.mccreatecalc.application.repositories.NetworkRepository;
 import org.jellerijk.mccreatecalc.application.usecases.network.FetchNetworksInfoUseCase;
 import org.jellerijk.mccreatecalc.application.usecases.network.NetworkInfo;
 import org.jellerijk.mccreatecalc.entities.StressNetwork;
+import org.jellerijk.mccreatecalc.presentation.controllers.NetworkDetailsController;
 import org.jellerijk.mccreatecalc.presentation.controllers.NetworkListController;
+import org.jellerijk.mccreatecalc.util.fxlib.HBoxes;
 
 import java.util.List;
 
@@ -26,7 +30,9 @@ public class CalculatorTestApplication extends Application {
                         return List.of(new NetworkInfo("123", "Network 1"), new NetworkInfo("321", "Netwerker"));
                     }
                 }));
-        Scene scene = new Scene(controller.getView());
+        NetworkDetailsController detailsController = new NetworkDetailsController();
+        HBox container = HBoxes.aligned(Pos.CENTER_LEFT, 5, controller.getView(), detailsController.getView());
+        Scene scene = new Scene(container);
 
         stage.setScene(scene);
         stage.setTitle("Test application");
---
+++ src/main/java/org/jellerijk/mccreatecalc/presentation/interactor/NetworkDetailsInteractor.java
@@ -0,0 +1,15 @@
+package org.jellerijk.mccreatecalc.presentation.interactor;
+
+import org.jellerijk.mccreatecalc.presentation.model.NetworkDetailsModel;
+
+public class NetworkDetailsInteractor {
+    private final NetworkDetailsModel model;
+
+    public NetworkDetailsInteractor(NetworkDetailsModel model) {
+        this.model = model;
+    }
+
+    public void selectNetwork() {
+
+    }
+}
