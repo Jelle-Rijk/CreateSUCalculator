28591733bdd5dfbdd56d2cd0bd6635bfc7969e7b
Jelle Rijk <jellerijk@gmail.com>
2026-06-21 19:32:19 +0200

    Defined networkservice interface

---
+++ src/main/java/org/jellerijk/mccreatecalc/application/services/NetworkService.java
@@ -0,0 +1,27 @@
+package org.jellerijk.mccreatecalc.application.services;
+
+import org.jellerijk.mccreatecalc.entities.StressNetwork;
+
+import java.util.Optional;
+
+public interface NetworkService {
+    /**
+     * @param id The unique identifier for the network to retrieve.
+     * @return An optional containing the stress network or null.
+     */
+    Optional<StressNetwork> getById(String id);
+
+    /**
+     * @return The currently selected network.
+     */
+    Optional<StressNetwork> getSelectedNetwork();
+
+    /**
+     * Sets the currently selected network.
+     *
+     * @param network The network to set as selected.
+     */
+    void setSelectedNetwork(StressNetwork network);
+
+
+}
