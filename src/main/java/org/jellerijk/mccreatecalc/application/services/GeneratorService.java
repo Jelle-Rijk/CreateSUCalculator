745894eadd1278ef55be96a512d4d6ce1edaaaeb
Jelle Rijk <jellerijk@gmail.com>
2026-06-21 19:36:04 +0200

    Changed getByName to return an optional

--- src/main/java/org/jellerijk/mccreatecalc/application/services/GeneratorService.java
+++ src/main/java/org/jellerijk/mccreatecalc/application/services/GeneratorService.java
@@ -1,4 +1,14 @@
 package org.jellerijk.mccreatecalc.application.services;
 
+import org.jellerijk.mccreatecalc.entities.Generator;
+
+import java.util.Optional;
+
 public interface GeneratorService {
+    /**
+     * @param name The generator's unique name.
+     * @return The generator that has the <code>name</code>.
+     */
+    Optional<Generator> getByName(String name);
+
 }
