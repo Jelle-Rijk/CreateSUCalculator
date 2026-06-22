c926a35a4b03c8e130614a46eb75728a0543b15e
Jelle Rijk <jellerijk@gmail.com>
2026-06-22 20:46:54 +0200

    Implemented numerical TextField

--- src/main/java/org/jellerijk/mccreatecalc/util/fxlib/TextFields.java
+++ src/main/java/org/jellerijk/mccreatecalc/util/fxlib/TextFields.java
@@ -2,11 +2,24 @@ package org.jellerijk.mccreatecalc.util.fxlib;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.util.converter.IntegerStringConverter;
 
 public abstract class TextFields {
     public static TextField numericalField(IntegerProperty property, String... styleClasses) {
-     TextField txf = new TextField();
-     // TODO : implement filter
-     return txf;
+        TextField txf = new TextField();
+        txf.setTextFormatter(createPositiveIntegerFormatter(property));
+        txf.getStyleClass().addAll(styleClasses);
+        return txf;
+    }
+
+    public static TextFormatter<Integer> createPositiveIntegerFormatter(IntegerProperty valueProperty) {
+        TextFormatter<Integer> textFormatter = new TextFormatter<>(new IntegerStringConverter(), 0, change -> {
+            if (change.getControlNewText().matches("[0-9]*"))
+                return change;
+            return null;
+        });
+        textFormatter.valueProperty().bindBidirectional(valueProperty.asObject());
+        return textFormatter;
     }
 }
