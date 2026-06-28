c0b32d5a562f63bc061662265d86abcd8df8a1a0
Jelle Rijk <jellerijk@gmail.com>
2026-06-28 10:16:37 +0200

    Added future yet-to-be-implemented use cases.

---
+++ src/main/java/org/jellerijk/mccreatecalc/application/usecases/AddComponentGroupToSelectedNetworkUC.java
@@ -0,0 +1,6 @@
+package org.jellerijk.mccreatecalc.application.usecases;
+
+// TODO: implement
+// This use case should add a new component group to the selected network. Before we can do this, we need to know which component this group will represent. The amount, level, sails, rpm, etc. can all be set to their defaults.
+public class AddComponentGroupToSelectedNetworkUC {
+}
