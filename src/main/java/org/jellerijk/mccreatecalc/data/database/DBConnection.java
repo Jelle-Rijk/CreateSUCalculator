4bef612c70376ead09cbad33551ff27d7ed0bd00
Jelle Rijk <jellerijk@gmail.com>
2026-07-01 11:51:22 +0200

    Added autocommit option

--- src/main/java/org/jellerijk/mccreatecalc/data/database/DBConnection.java
+++ src/main/java/org/jellerijk/mccreatecalc/data/database/DBConnection.java
@@ -10,19 +10,29 @@ public abstract class DBConnection {
             .of(System.getProperty("user.home"), "JR_Custom_Tools", "CreateCalculator", "database.sqlite");
     public static final String DATABASE_URL = String.format("jdbc:sqlite:%s", DATABASE_LOCATION);
 
+
     /**
-     * Gets a new Connection
-     *
+     * @param autocommit Whether the connection should autocommit or not.
      * @return Connection to the database.
      */
-    public static Connection getConnection() {
+    public static Connection getConnection(boolean autocommit) {
         try {
             Connection conn = DriverManager.getConnection(DATABASE_URL);
             if (conn == null)
                 throw new IllegalStateException("Could not get connection to Database.");
+            conn.setAutoCommit(autocommit);
             return conn;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Gets a new Connection with autocommit set to true.
+     *
+     * @return Connection to the database.
+     */
+    public static Connection getConnection() {
+        return getConnection(true);
     }
 }
