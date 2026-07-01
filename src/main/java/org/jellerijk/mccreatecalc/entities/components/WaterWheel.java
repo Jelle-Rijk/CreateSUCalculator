079c7a15546e0716ae10bab7ab63e066b9b696a7
Jelle Rijk <jellerijk@gmail.com>
2026-07-01 11:51:10 +0200

    WaterWheel now can only be built using WaterWheelType

--- src/main/java/org/jellerijk/mccreatecalc/entities/components/WaterWheel.java
+++ src/main/java/org/jellerijk/mccreatecalc/entities/components/WaterWheel.java
@@ -4,24 +4,15 @@ public class WaterWheel extends BaseComponent implements Generator {
 
     private final int suGeneration;
     private final int rpm;
+    private final WaterWheelType size;
 
     public WaterWheel(WaterWheelType waterWheelType) {
         super(waterWheelType.getName(), waterWheelType.getImg());
+        super.validateRpm(waterWheelType.getRpm());
+        validateSU(waterWheelType.getSuProduction());
         this.rpm = waterWheelType.getRpm();
         this.suGeneration = waterWheelType.getSuProduction();
-    }
-
-    public WaterWheel(String name, String img, int suGeneration, int rpm) {
-        super(name, img);
-        validateSU(suGeneration);
-        validateRPM(rpm);
-        this.rpm = rpm;
-        this.suGeneration = suGeneration;
-    }
-
-    private void validateRPM(int rpm) {
-        if (rpm < 0)
-            throw new IllegalArgumentException("RPM cannot be a negative number.");
+        this.size = waterWheelType;
     }
 
     //===== Public methods =====
@@ -47,41 +38,8 @@ public class WaterWheel extends BaseComponent implements Generator {
             throw new IllegalArgumentException("Stress unit generation needs to be a positive integer.");
     }
 
-    public static final class Builder {
-        private String img;
-        private String name;
-        private int suGeneration;
-        private int rpm;
 
-        private Builder() {
-        }
-
-        public static Builder aConstantGenerator() {
-            return new Builder();
-        }
-
-        public WaterWheel build() {
-            return new WaterWheel(name, img, suGeneration, rpm);
-        }
-
-        public Builder withImg(String img) {
-            this.img = img;
-            return this;
-        }
-
-        public Builder withName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder withSuGeneration(int suGeneration) {
-            this.suGeneration = suGeneration;
-            return this;
-        }
-
-        public Builder withRpm(int rpm) {
-            this.rpm = rpm;
-            return this;
-        }
+    public WaterWheelType getSize() {
+        return size;
     }
 }
