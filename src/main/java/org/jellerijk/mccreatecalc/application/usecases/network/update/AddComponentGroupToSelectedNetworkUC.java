d77df7ccf234b8c64171c2b62dea8a80f10ff245
Jelle Rijk <jellerijk@gmail.com>
2026-07-03 15:55:21 +0200

    Removed old todo.

--- src/main/java/org/jellerijk/mccreatecalc/application/usecases/network/update/AddComponentGroupToSelectedNetworkUC.java
+++ src/main/java/org/jellerijk/mccreatecalc/application/usecases/network/update/AddComponentGroupToSelectedNetworkUC.java
@@ -11,8 +11,6 @@ import org.jellerijk.mccreatecalc.entities.components.*;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: implement
-// This use case should add a new component group to the selected network. Before we can do this, we need to know which component this group will represent. The amount, level, sails, rpm, etc. can all be set to their defaults.
 public class AddComponentGroupToSelectedNetworkUC implements UseCase<AddComponentGroupRequest, ComponentGroupDTO> {
 
     private final ComponentService componentService;
