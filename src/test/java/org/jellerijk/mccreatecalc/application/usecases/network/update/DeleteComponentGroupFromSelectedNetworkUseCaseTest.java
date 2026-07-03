88b1cdd30d5948fbddf6968f344f4e2ee7ce965a
Jelle Rijk <jellerijk@gmail.com>
2026-07-03 17:45:49 +0200

    Added delete() and getNetworkIdForGroup()

--- src/test/java/org/jellerijk/mccreatecalc/application/usecases/network/update/DeleteComponentGroupFromSelectedNetworkUseCaseTest.java
+++ src/test/java/org/jellerijk/mccreatecalc/application/usecases/network/update/DeleteComponentGroupFromSelectedNetworkUseCaseTest.java
@@ -4,7 +4,6 @@ import org.jellerijk.mccreatecalc.application.gateways.SelectedNetworkData;
 import org.jellerijk.mccreatecalc.application.services.ComponentGroupService;
 import org.jellerijk.mccreatecalc.application.services.NetworkService;
 import org.jellerijk.mccreatecalc.application.services.NetworkServiceImpl;
-import org.jellerijk.mccreatecalc.application.services.SelectedNetworkPublisher;
 import org.jellerijk.mccreatecalc.data.local.SelectedNetwork;
 import org.jellerijk.mccreatecalc.entities.StressNetwork;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,10 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteComponentGroupFromSelectedNetworkUseCaseTest {
