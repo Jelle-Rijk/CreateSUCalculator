7a500bdfcfb878432dc6f27fd022742132862968
Jelle Rijk <jellerijk@gmail.com>
2026-07-04 11:44:22 +0200

    Balance label now properly binds the text colour to its number property.

--- src/main/java/org/jellerijk/mccreatecalc/util/fxlib/Labels.java
+++ src/main/java/org/jellerijk/mccreatecalc/util/fxlib/Labels.java
@@ -6,7 +6,6 @@ import javafx.beans.value.ObservableIntegerValue;
 import javafx.beans.value.ObservableStringValue;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
 public abstract class Labels {
     public static Label boundLabel(ObservableStringValue text, String... styleClasses) {
@@ -23,15 +22,11 @@ public abstract class Labels {
         return lbl;
     }
 
-    public static Label balanceLabel(IntegerProperty number, String... styleClasses) {//TODO bidirectional binding
+    public static Label balanceLabel(IntegerProperty number, String... styleClasses) {
         Label lbl = new Label();
         lbl.textProperty().bind(number.asString());
         lbl.textFillProperty()
                 .bind(Bindings.createObjectBinding(() -> number.get() == 0 ? Color.BLACK : number.get() < 0 ? Color.RED : Color.GREEN, number));
-        number.addListener((_, _, newNumber) -> {
-            int value = newNumber.intValue();
-            lbl.setTextFill(Paint.valueOf(value == 0 ? "Black" : value < 0 ? "Red" : "Green"));
-        });
         lbl.getStyleClass().addAll(styleClasses);
         return lbl;
     }
