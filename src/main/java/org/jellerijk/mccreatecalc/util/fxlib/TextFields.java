3405542a186f63c917e202c50da213e2605aad9e
Jelle Rijk <jellerijk@gmail.com>
2026-07-04 12:17:30 +0200

    Fix: NumericalField now properly handles 0 and null values.

--- src/main/java/org/jellerijk/mccreatecalc/util/fxlib/TextFields.java
+++ src/main/java/org/jellerijk/mccreatecalc/util/fxlib/TextFields.java
@@ -1,30 +1,38 @@
 package org.jellerijk.mccreatecalc.util.fxlib;
 
 import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.util.StringConverter;
 
 public abstract class TextFields {
-    public static TextField bidirectionallyBound(StringProperty property, String... styleClasses) {
-        TextField txf = new TextField();
-        txf.textProperty().bindBidirectional(property);
-        txf.getStyleClass().addAll(styleClasses);
-        return txf;
-    }
-
     public static TextField numericalField(IntegerProperty intProperty, String... styleClasses) {
-        StringProperty intPropertyAsString = new SimpleStringProperty();
-        intProperty.addListener((_, _, integer) -> intPropertyAsString.set(integer.toString()));
-        intPropertyAsString.addListener((_, _, input) -> intProperty.set(Integer.parseInt(input)));
+        TextField txf = styledTxf(styleClasses);
 
-        TextField txf = bidirectionallyBound(intPropertyAsString, styleClasses);
-        txf.setTextFormatter(new TextFormatter<>(change -> {
+        StringConverter<Integer> blankIsZero = new StringConverter<>() {
+            @Override
+            public String toString(Integer integer) {
+                return integer == null ? "0" : Integer.toString(integer);
+            }
+
+            @Override
+            public Integer fromString(String s) {
+                return s == null || s.isBlank() ? 0 : Integer.parseInt(s);
+            }
+        };
+        TextFormatter<Integer> formatter = new TextFormatter<>(blankIsZero, intProperty.get(), change -> {
             if (change.getControlNewText().matches("[0-9]*"))
                 return change;
             return null;
-        }));
+        });
+        txf.setTextFormatter(formatter);
+        formatter.valueProperty().bindBidirectional(intProperty.asObject());
+        return txf;
+    }
+
+    private static TextField styledTxf(String... styleClasses) {
+        TextField txf = new TextField();
+        txf.getStyleClass().addAll(styleClasses);
         return txf;
     }
 }
