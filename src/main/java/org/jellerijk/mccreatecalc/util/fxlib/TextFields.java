fb4773ae4b509522b467f39a6a98c5b97ab48304
Jelle Rijk <jellerijk@gmail.com>
2026-07-04 11:45:51 +0200

    NumericalField now uses listeners and an internal StringProperty rather than a binding.

--- src/main/java/org/jellerijk/mccreatecalc/util/fxlib/TextFields.java
+++ src/main/java/org/jellerijk/mccreatecalc/util/fxlib/TextFields.java
@@ -1,10 +1,10 @@
 package org.jellerijk.mccreatecalc.util.fxlib;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
-import javafx.util.converter.NumberStringConverter;
 
 public abstract class TextFields {
     public static TextField bidirectionallyBound(StringProperty property, String... styleClasses) {
@@ -14,15 +14,17 @@ public abstract class TextFields {
         return txf;
     }
 
-    public static TextField numericalField(IntegerProperty property, String... styleClasses) {
-        TextField txf = new TextField();
-        txf.textProperty().bindBidirectional(property, new NumberStringConverter());
+    public static TextField numericalField(IntegerProperty intProperty, String... styleClasses) {
+        StringProperty intPropertyAsString = new SimpleStringProperty();
+        intProperty.addListener((_, _, integer) -> intPropertyAsString.set(integer.toString()));
+        intPropertyAsString.addListener((_, _, input) -> intProperty.set(Integer.parseInt(input)));
+
+        TextField txf = bidirectionallyBound(intPropertyAsString, styleClasses);
         txf.setTextFormatter(new TextFormatter<>(change -> {
             if (change.getControlNewText().matches("[0-9]*"))
                 return change;
             return null;
         }));
-        txf.getStyleClass().addAll(styleClasses);
         return txf;
     }
 }
